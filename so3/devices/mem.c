639426fe310db047a1d2ac834d60db8adb1b305f
Clement Dieperink <clement.dieperink@heig-vd.ch>
2025-02-19 09:42:35 +0100

    split mapping into before/in/after RAM for better cache setup

--- so3/devices/mem.c
+++ so3/devices/mem.c
@@ -9,16 +9,51 @@
 
 static void *mem_mmap(int fd, addr_t virt_addr, uint32_t page_count, off_t offset)
 {
+	const size_t MEM_START = mem_info.phys_base;
+	const size_t MEM_END = mem_info.phys_base + mem_info.size;
 	pcb_t *pcb;
-	bool no_cache;
+	addr_t next_phys_addr = offset;
+	addr_t next_virt_addr = virt_addr;
+	size_t remaining_size = page_count * PAGE_SIZE;
+	size_t map_size;
+
+	/* Detect physical or virtual address overflow */
+	if (((offset + remaining_size) < offset)
+	    || ((virt_addr + remaining_size) < virt_addr))
+		return MAP_FAILED;
+
+	if (!virt_addr)
+		return MAP_FAILED;
 
 	pcb = current()->pcb;
+	BUG_ON(pcb == NULL);
 
-	/* Use cache only if the target offset is in RAM */
-	no_cache = (offset < mem_info.phys_base) &&
-	           ((mem_info.phys_base + mem_info.size) <= offset);
+	/* Mapping before the RAM */
+	if ((remaining_size > 0) && (next_phys_addr < MEM_START)) {
+		map_size = min(remaining_size, MEM_START - next_phys_addr);
+		create_mapping(pcb->pgtable, next_virt_addr, next_phys_addr,
+		               map_size, true);
 
-	create_mapping(pcb->pgtable, virt_addr, offset, page_count * PAGE_SIZE, no_cache);
+		next_virt_addr += map_size;
+		next_phys_addr += map_size;
+		remaining_size -= map_size;
+	}
+
+	/* Mapping in the RAM */
+	if ((remaining_size > 0) && (next_phys_addr < MEM_END)) {
+		map_size = min(remaining_size, MEM_END - next_phys_addr);
+		create_mapping(pcb->pgtable, next_virt_addr, next_phys_addr,
+		               map_size, false);
+
+		next_virt_addr += map_size;
+		next_phys_addr += map_size;
+		remaining_size -= map_size;
+	}
+
+	/* Mapping after the RAM */
+	if (remaining_size > 0)
+		create_mapping(pcb->pgtable, next_virt_addr, next_phys_addr,
+		               remaining_size, true);
 
 	return (void *)virt_addr;
 }
