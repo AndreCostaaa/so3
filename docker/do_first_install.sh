8a1a1eeee01bd30cdb96be453b0286a775ae6942
André <andre_miguel_costa@hotmail.com>
2025-03-08 16:41:02 +0100

    fix: use create img and create ramfs scripts

--- docker/do_first_install.sh
+++ docker/do_first_install.sh
@@ -1,47 +1,14 @@
 #!/bin/sh
 
-# 1. Create FileSystem Image
-dd if=/dev/zero of=filesystem/sdcard.img.virt32 bs=256M count=1
-DEVNAME=$(losetup --partscan --find --show filesystem/sdcard.img.virt32)
-(echo o; echo n; echo p; echo; echo; echo; echo t; echo c; echo w) | fdisk $DEVNAME;
+cd filesystem; ./create_img.sh virt32 && cd ..
+cd rootfs; ./create_ramfs.sh && cd ..
 
-mkfs.fat -F32 -v ${DEVNAME}1
-mkfs.ext4 ${DEVNAME}2
-
-
-# 2. Create Rootfs
-
-mkdir /so3/tmp
-START_SECTOR=2048
-PARTITION_SIZE=16M
-PARTITION_TYPE=c
-PARTITION="/so3/tmp/partition.img"
-
-# Create image first
-IMAGE_NAME="rootfs/rootfs.fat"
-dd if=/dev/zero of="${IMAGE_NAME}" count=${START_SECTOR} status=none
-
-# Append the formatted partition
-dd if=/dev/zero of="${PARTITION}" bs=${PARTITION_SIZE} count=1 status=none
-mkfs.vfat "${PARTITION}" > /dev/null
-dd if="${PARTITION}" status=none >> "${IMAGE_NAME}"
-
-# Set the partition table
-sfdisk "${IMAGE_NAME}" <<EOF
-${START_SECTOR}, ${PARTITION_SIZE}, ${PARTITION_TYPE}
-EOF
-
-rm -rf /so3/tmp
-
-mkdir filesystem/fs
-
-# 2. Make u-boot
 mkimage -f target/virt32_lvperf.its target/virt32_lvperf.itb;
 
 mount $(losetup --partscan --find --show filesystem/sdcard.img.virt32)p1 filesystem/fs
 
 cp target/virt32_lvperf.itb filesystem/fs
-cp u-boot/uEnv.d/uEnv_virt32.txt fs/uEnv.txt
+cp u-boot/uEnv.d/uEnv_virt32.txt filesystem/fs/uEnv.txt
 
 umount filesystem/fs
 losetup -D
