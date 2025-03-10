40e230af91ac8755ff1b33050de9fcd320a213ab
André <andre_miguel_costa@hotmail.com>
2025-03-10 18:10:30 +0100

    refactor: move filesystem and ramfs setup to their own scripts

---
+++ docker/scripts/setup_filesystem.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+target_path="/persistence/sdcard.img.virt32"
+
+if [ -f "$target_path" ]; then
+    echo "Found existing filesystem image in $target_path."
+else 
+  echo "No existing rootfs image found in $target_path. Creating new one..."
+  dd_size=256M
+  dd if=/dev/zero of=$target_path bs="$dd_size" count=1
+  devname=$(losetup --partscan --find --show $target_path)
+  
+  devname=${devname#"/dev/"}
+  
+  (echo o; echo n; echo p; echo; echo; echo; echo t; echo c; echo w) | fdisk /dev/"$devname"
+  
+  if [[ "$devname" = *[0-9] ]]; then
+      export devname="${devname}p"
+  fi
+  
+  mkfs.fat -F32 -v /dev/"$devname"p1
+  mkfs.ext4 /dev/"$devname"p2
+  losetup -D
+fi
---
+++ docker/scripts/setup_ramfs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+target_path="/persistence/rootfs.fat.virt32"
+
+if [ -f "$target_path" ]; then
+    echo "Found existing rootfs image in $target_path."
+else 
+  echo "No existing rootfs image found in $target_path. Creating new one..."
+  start_sector=2048
+  partition_size=16M # This image will be copied into the .itb and written to SD card image so it must be small enough
+  partition_type=c
+  tmp_dir=$(mktemp -d -t so3-rootfs-XXXXXXXX)
+  partition="${tmp_dir}/partition.img"
+  
+  # Create image first
+  dd if=/dev/zero of="${target_path}" count=${start_sector} status=none
+  
+  # Append the formatted partition
+  dd if=/dev/zero of="${partition}" bs=${partition_size} count=1 status=none
+  mkfs.vfat "${partition}"
+  dd if="${partition}" status=none >> "${target_path}"
+    
+  # Set the partition table
+  sfdisk "${target_path}" <<EOF
+${start_sector}, ${partition_size}, ${partition_type}
+EOF
+    
+  # Delete temporary directory
+  rm -r "${tmp_dir}"
+fi
