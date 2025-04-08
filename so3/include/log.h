97247609f843984bd62e958843228cca69ef861c
André <andre_miguel_costa@hotmail.com>
2025-04-08 19:35:02 +0200

    refactor(log): uppercase log level

--- so3/include/log.h
+++ so3/include/log.h
@@ -27,47 +27,51 @@
 #define LOG_LEVEL_DEBUG 5
 #define LOG_LEVEL_TRACE 6
 
+#ifndef CONFIG_LOG_LEVEL
+#define CONFIG_LOG_LEVEL LOG_LEVEL_INFO
+#endif
+
 #define LOG(level, fmt, ...)                                            \
 	lprintk("[" #level "] <%s:%d> " fmt "\r\n", __func__, __LINE__, \
 		##__VA_ARGS__)
 
 #if CONFIG_LOG_LEVEL >= LOG_LEVEL_CRITICAL
-#define LOG_CRITICAL(fmt, ...) LOG(critical, fmt, ##__VA_ARGS__)
+#define LOG_CRITICAL(fmt, ...) LOG(CRITICAL, fmt, ##__VA_ARGS__)
 
 #else
 #define LOG_CRITICAL(fmt, ...)
 #endif
 
 #if CONFIG_LOG_LEVEL >= LOG_LEVEL_ERROR
-#define LOG_ERROR(fmt, ...) LOG(error, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) LOG(ERROR, fmt, ##__VA_ARGS__)
 
 #else
 #define LOG_ERROR(fmt, ...)
 #endif
 
 #if CONFIG_LOG_LEVEL >= LOG_LEVEL_WARNING
-#define LOG_WARNING(fmt, ...) LOG(warning, fmt, ##__VA_ARGS__)
+#define LOG_WARNING(fmt, ...) LOG(WARNING, fmt, ##__VA_ARGS__)
 
 #else
 #define LOG_WARNING(fmt, ...)
 #endif
 
 #if CONFIG_LOG_LEVEL >= LOG_LEVEL_INFO
-#define LOG_INFO(fmt, ...) LOG(info, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) LOG(INFO, fmt, ##__VA_ARGS__)
 
 #else
 #define LOG_INFO(fmt, ...)
 #endif
 
 #if CONFIG_LOG_LEVEL >= LOG_LEVEL_DEBUG
-#define LOG_DEBUG(fmt, ...) LOG(debug, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) LOG(DEBUG, fmt, ##__VA_ARGS__)
 
 #else
 #define LOG_DEBUG(fmt, ...)
 #endif
 
 #if CONFIG_LOG_LEVEL >= LOG_LEVEL_TRACE
-#define LOG_TRACE(fmt, ...) LOG(trace, fmt, ##__VA_ARGS__)
+#define LOG_TRACE(fmt, ...) LOG(TRACE, fmt, ##__VA_ARGS__)
 
 #else
 #define LOG_TRACE(fmt, ...)
